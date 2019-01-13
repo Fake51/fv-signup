6575e1577ad3931f73fa8064105260138e6a3bdb
Mikkel Westh <westh_mikkel@hotmail.com>
2019-01-13 17:18:36 +0100

    CHANGED default GDS shifts to 1 and in a way that actually works

--- templates/fastaval-deltager-2019/tilmelding_8_gds.php
+++ templates/fastaval-deltager-2019/tilmelding_8_gds.php
@@ -207,11 +207,11 @@
                     <h3><?php __etm('gds_how_many_headline');?></h3>
                     <p><?php __etm('gds_how_many_text');?>
                     <?php
+                        if (!isset($_SESSION['customer']['desired_diy_shifts'])) $_SESSION['customer']['desired_diy_shifts'] = 1;
                 			renderFieldByType(array(
                     			'id'=>'desired_diy_shifts',
                     			'input-type'=>'text',
                     			'input-name'=>'desired_diy_shifts',
-                    			'default-value' => 3,
                     			'text' => __tm('gds_angiv_antal'),
                 			));
             			?>
