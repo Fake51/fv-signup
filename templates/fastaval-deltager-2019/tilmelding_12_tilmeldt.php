a78b5ef5bcf00518a6ad067802ac23cae40e6528
Mikkel Westh <westh_mikkel@hotmail.com>
2019-03-13 12:44:46 +0100

    CHANGED ID(s) for scenario writing competition

--- templates/fastaval-deltager-2019/tilmelding_12_tilmeldt.php
+++ templates/fastaval-deltager-2019/tilmelding_12_tilmeldt.php
@@ -454,7 +454,9 @@
     			*/
     				
     			if ($customer['scenarieskrivningskonkurrence']==1)
-    				$signup_data['activity'][] = array('schedule_id' => 2,'priority' => 1,'type' => "spiller");
+    				$signup_data['activity'][] = array('schedule_id' => 205,'priority' => 1,'type' => "spiller");
+    				$signup_data['activity'][] = array('schedule_id' => 206,'priority' => 1,'type' => "spiller");
+    				$signup_data['activity'][] = array('schedule_id' => 207,'priority' => 1,'type' => "spiller");
     				
     			if ($customer['boardgame_competition']==1)
     				$signup_data['activity'][] = array('schedule_id' => 170,'priority' => 1,'type' => "spiller");
